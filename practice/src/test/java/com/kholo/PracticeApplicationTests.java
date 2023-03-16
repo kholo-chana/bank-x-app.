4156796c87636caaf9e64af6f9a3bf7fa03c87f5
kholo-chana <phaswane8@gmail.com>
2023-03-16 07:57:28 +0200

    Bank X application

---
+++ practice/src/test/java/com/kholo/PracticeApplicationTests.java
@@ -0,0 +1,31 @@
+//package com.kholo;
+//
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//
+//
+//class PracticeApplicationTests {
+//
+//
+//    @Test
+//     void testName(){
+//
+//        Message msg = new Message();
+//        Assertions.assertEquals("Hello Daily Code Buffer!",msg.getMessage("Daily Code Buffer"));
+//    }
+//
+//    @Test
+//     void testNameBlank(){
+//        Message msg = new Message();
+//        Assertions.assertEquals("Please Provide a name!",msg.getMessage(""));
+//
+//    }
+//
+//    @Test
+//     void testNameNull(){
+//        Message msg = new Message();
+//        Assertions.assertEquals("Please Provide a name!",msg.getMessage(null));
+//
+//    }
+//
+//}
